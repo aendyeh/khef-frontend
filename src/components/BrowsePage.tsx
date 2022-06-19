10ea762728df4f096b9d6be61eae12d4701aaf4f
aendyeh <107498945+aendyeh@users.noreply.github.com>
2022-06-19 15:29:37 +0300

    Add Pills

--- src/components/BrowsePage.tsx
+++ src/components/BrowsePage.tsx
@@ -1,6 +1,17 @@
+import Pills from "./shared/Pills"
+import Search from "./shared/Search"
+
 function BrowsePage() {
+  function pillItems() {
+    return []
+  }
+
   return (
-    <div>Browse . .. ...</div>
+    <article className="browsable-content">
+      <Search onSubmit={() => console.warn("TODO")} hasButton={false} />
+      <Pills items={pillItems()} />
+      <h1>Results</h1>
+    </article>
   )
 }
 
