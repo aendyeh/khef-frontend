89052694a5cfa72d8b0842a0c15cd251fa1f61e0
aendyeh <107498945+aendyeh@users.noreply.github.com>
2022-06-19 15:16:45 +0300

    Styles + conditional rendering

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,24 @@
+import './Search.scss'
+
 type Props = {
   onSubmit: () => void
+  hasButton?: boolean
 }
 
-function Search({ onSubmit }: Props) {
+function Search({ onSubmit, hasButton }: Props) {
+  function isButtonRendered() {
+    return hasButton === undefined 
+      ? true 
+      : hasButton
+  }
+
   return (
     <div className='search-container'>
       <form name='search' onSubmit={() => onSubmit()}>
         <input type='text' placeholder='Find by ingredient(s)' />
-        <button type='submit'>Find</button>
+        {isButtonRendered() && 
+          <button type='submit'>Find</button>
+        }
       </form>
     </div>
   )
