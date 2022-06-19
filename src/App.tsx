a99229d7b27474841031987137cdbca608536c16
aendyeh <107498945+aendyeh@users.noreply.github.com>
2022-06-19 12:18:53 +0300

    Add a route for funzies

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ function App() {
         <Route path='/'>
           <Route index element={<StartPage />} />
         </Route>
+        <Route path='/browse'>
+          <Route index element={<div>Hello!</div>} />
+        </Route>
       </Routes>
     </div>
   );
